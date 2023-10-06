f1ba6b220dea6b49527dccbcac4050c94d7f15ef
ecorara446 <ecorara446@g.educaand.es>
2023-10-06 09:22:18 +0200

    ahora si

--- src/main/java/EjerciciosUT3/Ej03.java
+++ src/main/java/EjerciciosUT3/Ej03.java
@@ -17,5 +17,25 @@ public class Ej03 {
 
         System.out.println(resultado);
         System.out.println("----------------------");
+        
+        int x = 10;
+        int y = (x > 9) ? 100 : 200;
+        System.out.println(y);
+        System.out.println("----------------------");
+        
+        int publico = 19500;
+    	int vendidas = 19000;
+	int aforo = 2000;
+  	String status = (publico < aforo) ? "Cabe mas gente": 
+        (vendidas < aforo) ? "Aun no hemos vendido todo" : "Esta todo vendido";
+        System.out.println(status);
+
+        /*
+        valor1
+        ----------------------
+        100
+        ----------------------
+        Esta todo vendido
+        */
     }
 }
